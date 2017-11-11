195bc8565886a4734990dea8845b7b6187a1a4ef
Kyle Wright <krwright@umass.edu>
2017-11-11 22:48:30 +0000

    Add files via upload

---
+++ color_detection.py
@@ -0,0 +1,112 @@
+# import the necessary packages
+from imutils.video import VideoStream
+from collections import deque 
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-b", "--buffer", type=int, default=64,
+	help="max buffer size")
+args = vars(ap.parse_args())
+
+
+
+# define the lower and upper boundaries of the selected color
+# in the HSV color space, then initialize the
+# list of tracked points
+
+#tuple defined as Blue,Green,Red ---->
+colorLower = (30, 0, 0)
+colorUpper = (255, 200, 200)
+pts = deque(maxlen=args["buffer"])
+
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=800)
+
+	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+	# construct a mask for the color "green", then perform -----------
+	# a series of dilations and erosions to remove any small
+	# blobs left in the mask
+	mask = cv2.inRange(hsv, colorLower, colorUpper)
+	mask = cv2.erode(mask, None, iterations=5)
+	mask = cv2.dilate(mask, None, iterations=5)
+
+	# find contours in the mask and initialize the current
+	# (x, y) center of the subject
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		# only proceed if the radius meets a minimum size
+		if radius > 10:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+ 
+	# update the points queue
+	pts.appendleft(center)
+
+	# loop over the set of tracked points
+	for i in xrange(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+ 
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+ 
+	# show the frame to our screen
+	cv2.imshow("Frame", frame)
+	cv2.imshow("Mask", mask)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
+
+
+
