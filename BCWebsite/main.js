3dfee8887207baffed0a8f323ace1eb450b8dbb0
Steven <stevengurney14@gmail.com>
2017-12-10 10:44:26 -0500

    Combined into static scrolling page

--- BCWebsite/main.js
+++
@@ -1,8 +0,0 @@
-function changeRes(){
-  var x = document.getElementById('non_rep');
-    if (x.className === "nav_container") {
-      x.className += "_responsive";
-    } else {
-      x.className = "nav_container"
-    }
-}
